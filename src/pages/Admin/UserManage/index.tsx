ea85e8afeb8b9d8d438e7ed3d3e4e8981aa07707
qianye <1764402924@qq.com>
2023-12-19 01:12:25 +0800

    用户管理页面

---
+++ src/pages/Admin/UserManage/index.tsx
@@ -0,0 +1,188 @@
+import type { ActionType, ProColumns } from '@ant-design/pro-components';
+import { ProTable, TableDropdown } from '@ant-design/pro-components';
+import { useRef } from 'react';
+import {searchUsers} from "@/services/ant-design-pro/api";
+import {CurrentUser} from "@/pages/AccountCenter/data";
+import {Image} from "antd";
+export const waitTimePromise = async (time: number = 100) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(true);
+    }, time);
+  });
+};
+
+export const waitTime = async (time: number = 100) => {
+  await waitTimePromise(time);
+};
+
+// @ts-ignore
+const columns: ProColumns<CurrentUser>[] = [
+  {
+    dataIndex: 'id',  //对应后端的列名
+    valueType: 'indexBorder',
+    width: 48,
+  },
+  {
+    title: '用户名',
+    dataIndex: 'username',
+    copyable: true,
+    ellipsis: true,
+  },
+  {
+    title: '账户名',
+    dataIndex: 'userAccount',
+    copyable: true,
+    ellipsis: true,
+  },
+  {
+    title: '头像',
+    dataIndex: 'avatarUrl',
+    render:(_, record) => (
+      <div>
+        <Image src={record.avatarUrl} width={100} height={100} />
+      </div>
+    ),
+  },
+  {
+    title: '性别',
+    dataIndex: 'gender',
+    copyable: true,
+    ellipsis: true,
+  },
+  {
+    title: '电话',
+    dataIndex: 'phone',
+    copyable: true,
+  },
+  {
+    title: '邮箱',
+    dataIndex: 'email',
+    copyable: true,
+  },
+  {
+    title: '状态',
+    dataIndex: 'userStatus',
+    ellipsis: true,
+
+  },
+  {
+    title: '角色',
+    dataIndex: 'userRole',
+    ellipsis: true,
+    valueType: 'select',
+    valueEnum: {
+      0: { text: '普通用户', status :'Default'},
+      1: {
+        text: '管理员',
+        status:'Success'
+      }
+    },
+  },
+  {
+    title: '创建时间',
+    dataIndex: 'createTime',
+    valueType: 'dateRange',
+    copyable: true,
+  },
+
+
+  // {
+  //   disable: true,
+  //   title: '标签',
+  //   dataIndex: 'labels',
+  //   search: false,
+  //   renderFormItem: (_, { defaultRender }) => {
+  //     return defaultRender(_);
+  //   },
+  //   render: (_, record) => (
+  //     <Space>
+  //       {record.labels.map(({ name, color }) => (
+  //         <Tag color={color} key={name}>
+  //           {name}
+  //         </Tag>
+  //       ))}
+  //     </Space>
+  //   ),
+  // },
+  {
+    title: '操作',
+    valueType: 'option',
+    key: 'option',
+    render: (text, record, _, action) => [
+      <a
+        key="editable"
+        onClick={() => {
+          action?.startEditable?.(record.id);
+        }}
+      >
+        编辑
+      </a>,
+      <a href={record.url} target="_blank" rel="noopener noreferrer" key="view">
+        查看
+      </a>,
+      <TableDropdown
+        key="actionGroup"
+        onSelect={() => action?.reload()}
+      />,
+    ],
+  },
+];
+
+export default () => {
+  const actionRef = useRef<ActionType>();
+  return (
+    <ProTable<CurrentUser>
+      columns={columns}
+      actionRef={actionRef}
+      cardBordered
+      request={async (params, sort, filter) => {
+        console.log(sort, filter);
+        const userList = await searchUsers();
+        return {
+          data: userList
+        }
+      }}
+      editable={{
+        type: 'multiple',
+      }}
+      columnsState={{
+        persistenceKey: 'pro-table-singe-demos',
+        persistenceType: 'localStorage',
+        defaultValue: {
+          option: { fixed: 'right', disable: true },
+        },
+        onChange(value) {
+          console.log('value: ', value);
+        },
+      }}
+      rowKey="id"
+      search={{
+        labelWidth: 'auto',
+      }}
+      options={{
+        setting: {
+          listsHeight: 400,
+        },
+      }}
+      form={{
+        // 由于配置了 transform，提交的参与与定义的不同这里需要转化一下
+        syncToUrl: (values, type) => {
+          if (type === 'get') {
+            return {
+              ...values,
+              created_at: [values.startTime, values.endTime],
+            };
+          }
+          return values;
+        },
+      }}
+      pagination={{
+        pageSize: 5,
+        onChange: (page) => console.log(page),
+      }}
+      dateFormatter="string"
+      headerTitle="高级表格"
+    />
+  );
+};
